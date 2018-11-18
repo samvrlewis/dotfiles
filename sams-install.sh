eea1324f209a47226b2538860fbe232136b9df8f
Sam Lewis <sam.vr.lewis@gmail.com>
2018-11-19 10:14:07 +1100

    update to initial install script

--- sams-install.sh
+++ sams-install.sh
@@ -9,7 +9,13 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo dnf install code tmux htop vim cmake make
 
 # Install packages for dev
-sudo dnf install boost-static ffmpeg ffmpeg-devel gtk2-devel gcc-c++
+sudo dnf install boost-static ffmpeg ffmpeg-devel gtk2-devel gcc-c++ openssl-devel
+
+# Some more dev packages
+sudo dnf install docker
+
+# copy libcaffe2
+sudo cp /usr/local/lib/libcaffe2.so /usr/lib64/
 
 # Set tmux to use the tmux conf
 ln -s -f ~/.tmux/.tmux.conf .
